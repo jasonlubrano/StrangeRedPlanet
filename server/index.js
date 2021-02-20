33ff8f4b3f6d7db3ace7e945ca0bba8c5d9d1d8f
jasonlubrano <jalubrano1@gmail.com>
2021-02-20 00:55:22 -0700

    backend server up

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
+const util = require("util");
+const formidable = require('formidable');
+const app = express();
+const port = 3030;
+
+const server = http.createServer(function(req, res) {
+    let path = url.parse(req.url, true);
+    if(req.method.toUpperCase() == "POST") {
+        let form = new formidable.IncomingForm();
+        form.parse(req, function(err, fields, files) {
+            if(err) {
+                console.error(err.message);
+                return;
+            }
+            res.writeHead(200, "OK", {'Content-Type':'text/plain'});
+            res.write("POST output \n\n");
+            res.end(util.inspect({fields:fields, files:files}));
+        });
+    } else if (req.method.toUpperCase() == "GET" ) {
+        res.writeHead(200, "OK", {'Content-Type':'text/plain'});
+        res.write("GET output \n\n");
+        res.write( util.inspect(path.query) + "\n\n");
+        res.end("End of message to user");
+    }
+});
+
+server.listen(port, function() {
+    console.log("Listening on port 3030");
+});
